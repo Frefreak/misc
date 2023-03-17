7200aff4c71904e2dac80eb0eefd7ad2350b6303
Frefreak <frefreak.zxy@gmail.com>
2023-03-17 21:06:44 +0800

    change python3 to python

--- shell_upgrade.sh
+++ shell_upgrade.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 tmux select-pane -t "$1"
-tmux send-keys 'python3 -c ' "'" 'import pty; pty.spawn("/bin/bash")' "'" enter
+tmux send-keys 'python -c ' "'" 'import pty; pty.spawn("/bin/bash")' "'" enter
 tmux send-keys C-z
 tmux send-keys 'stty raw -echo; fg' enter
 tmux send-keys 'export TERM=xterm-256color' enter enter
