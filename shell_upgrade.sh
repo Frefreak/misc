7d29702b3c538a8175db3786b10fdb1e2cddc945
carsonzhu <carsonzhu@tencent.com>
2023-06-13 11:13:45 +0800

    make shell_upgrade more robust

--- shell_upgrade.sh
+++ shell_upgrade.sh
@@ -6,6 +6,9 @@ if [ -z "$1" ]; then
 fi
 tmux select-pane -t "$1"
 tmux send-keys 'python -c ' "'" 'import pty; pty.spawn("/bin/bash")' "'" enter
+sleep 1
 tmux send-keys C-z
-tmux send-keys 'stty raw -echo; fg' enter
-tmux send-keys 'export TERM=xterm-256color' enter enter
+sleep 1
+tmux send-keys 'stty raw -echo; stty size; fg' enter
+sleep 1
+tmux send-keys 'export SHELL=bash TERM=xterm-256color' enter enter
